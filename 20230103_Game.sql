cce0e34a1e71bd999b293be9d1f79f99916c1c39
Dima Ballanar <D.Ballanar@gmail.com>
2023-03-02 02:19:17 +0300

    update

--- 20230103_Game.sql
+++ 20230103_Game.sql
@@ -1,1 +1,10 @@
-Create database if not exists Game
+Create database if not exists Game;
+use game;
+
+create table Users(
+Id int primary key auto_increment,
+Name VARCHAR(20) NOT NULL,
+Email VARCHAR(40) NOT NULL,
+Password VARCHAR(255) NOT NULL,
+Status INT NOT NULL
+);
