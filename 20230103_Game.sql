0e3dc3394412edb1acee36df52bd0e53a13f59de
Dima Ballanar <D.Ballanar@gmail.com>
2023-03-02 02:42:14 +0300

    update

--- 20230103_Game.sql
+++ 20230103_Game.sql
@@ -8,3 +8,13 @@ Email VARCHAR(40) NOT NULL,
 Password VARCHAR(255) NOT NULL,
 Status INT NOT NULL
 );
+
+insert into users(name,email,password,status) values
+		('Dima', 'D.Ballanar@gmail.com', '123123qwqq', 5),
+        ('Martha', 'Martha@gmail.com', '1231231421', 4), 
+		('Konstantin', 'Konstantin@gmail.com', 'ggt20', 3),
+		('Kirill', 'Kirill@gmail.com', '[qwe21', 4), 
+		('Yana', 'Yana@gmail.com', 'qweqwe22', 5);        
+        select*from users;
+        
+        
