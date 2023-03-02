002600d893a21aba3575f832f1d273d457a826fe
Dima Ballanar <D.Ballanar@gmail.com>
2023-03-02 23:07:46 +0300

    next

---
+++ 20230302_TableAnimals.sql
@@ -0,0 +1,20 @@
+-- CREATE database if not exists studies;
+
+-- use studies;
+
+-- create table if not exists Animal(
+-- Id INT auto_increment not null,
+-- Name varchar(20) not null,
+-- Type varchar(20) not null,
+-- primary key(id)
+-- );
+
+-- rename table Animal to Animals;
+
+-- insert into animals(name,type) values ('кот', 'млекопитающее');
+insert into animals(name,type) values ('собака', 'млекопитающее');
+insert into animals(name,type) values ('кот', 'земноводное');
+insert into animals(name,type) values ('змея', 'земноводное');
+insert into animals(name,type) values ('корова', 'млекопитающее');
+SELECT LAST_INSERT_ID();
+select 'name','type' from animals;
