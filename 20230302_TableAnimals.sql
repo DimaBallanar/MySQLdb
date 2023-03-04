80bf6d2baf455169b84bd409e610476489158dc7
Dima Ballanar <D.Ballanar@gmail.com>
2023-03-04 10:41:38 +0300

    update

--- 20230302_TableAnimals.sql
+++ 20230302_TableAnimals.sql
@@ -1,20 +1,24 @@
--- CREATE database if not exists studies;
+CREATE database if not exists studies;
 
--- use studies;
+ use studies;
 
--- create table if not exists Animal(
--- Id INT auto_increment not null,
--- Name varchar(20) not null,
--- Type varchar(20) not null,
--- primary key(id)
--- );
+ create table if not exists Animal(
+ Id INT auto_increment not null,
+ Name varchar(20) not null,
+ Type varchar(20) not null,
+ primary key(id)
+ );
 
--- rename table Animal to Animals;
+ rename table Animal to Animals;
 
--- insert into animals(name,type) values ('кот', 'млекопитающее');
+ insert into animals(name,type) values ('кот', 'млекопитающее');
 insert into animals(name,type) values ('собака', 'млекопитающее');
 insert into animals(name,type) values ('кот', 'земноводное');
 insert into animals(name,type) values ('змея', 'земноводное');
 insert into animals(name,type) values ('корова', 'млекопитающее');
 SELECT LAST_INSERT_ID();
 select 'name','type' from animals;
+select*from animals;
+truncate table animals;
+select*from animals where type='млекопитающее' and id between 1 and 4 ;
+delete from animals where Id=2;
