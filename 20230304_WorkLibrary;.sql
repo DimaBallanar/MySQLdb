65eb569be4697052081fcd92ca187ab6865dd102
Dima Ballanar <D.Ballanar@gmail.com>
2023-03-04 11:16:26 +0300

    new table

---
+++ 20230304_WorkLibrary;.sql
@@ -0,0 +1,34 @@
+Create database if not exists Library;
+use Library;
+
+Create table if not exists test(
+Id Int auto_increment,
+Title varchar(50) not null,
+Author varchar(50) not null,
+Price decimal(8,2) not null,
+primary key(Id)
+);
+
+rename table test to book;
+alter table book add Amount int not null;
+
+insert into book(Title,Author,price,amount) values
+('Мастер и Маргарита', 'Булгаков М. А.', 67.99, 3),
+ ('Белая гвардия', 'Булгаков М. А.', 54.50, 5),
+ ('Идиот', 'Достоевский Ф. М.', 46.00, 10),
+ ('Братья Карамазовы', 'Достоевский Ф. М.', 79.99, 2),
+ ('Стихотворения и поэмы', 'Есенин С. А.', 65.99, 15);
+ 
+ select*from book;
+ truncate table book;
+ 
+ select Title as "книги" from book ;
+  select Title from book ;
+ 
+ select Author as "автор", Title as "название книги", price as "цена" from book;
+ select count(*) as "количество книг" from book;
+ select Min(price) as "минимальная цена" from book;
+ select Max(price) as "максимальная цена" from book;
+ select*from book order by id desc limit 1;
+ 
+ 
