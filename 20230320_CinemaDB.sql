035402f9a1f228da841767438079bfbcc1c9cbf2
Dima Ballanar <D.Ballanar@gmail.com>
2023-03-21 02:39:00 +0300

    step

--- 20230320_CinemaDB.sql
+++ 20230320_CinemaDB.sql
@@ -1,3 +1,10 @@
 create database Cinema;
 use cinema;
 
+create table movies(
+id INT primary key auto_increment,
+title varchar(40) not null,
+director varchar(40) not null,
+year int not null,
+length_minutes int not null
+);
